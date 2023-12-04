495fb6a324214d1c363585fd5d7396e9620825d8
MatthewPoliks <152823456+MatthewPoliks@users.noreply.github.com>
2023-12-03 16:17:46 -0800

    Add files via upload

---
+++ java.js
@@ -0,0 +1,31 @@
+// const treasure = (location) => {
+//     var bomb = Math.floor(Math.random() * 9)
+//     var treasure = Math.floor(Math.random() * 9)
+//     return alert("Here is the function!")
+// }
+// td onclick="Treasure()">?</td>
+// const treasure= (location) => {
+//     return alert(location)
+// }
+// <td id="0" onclick="treasure(0)">?</td>
+// 🌴
+// 💣
+// 🌈
+// document.getElementById(location).innerHTML ="🌴"
+// document.getElementById(location).innerHTML ="💣"
+// document.getElementById(location).innerHTML ="🌈"
+// <button type="button" onclick="location.reload()">Restart Game</button>
+// alert("HTML and JS are connected")
+const treasure = (location) => {
+    var bomb = Math.floor(Math.random() * 10)
+    var gold = Math.floor(Math.random() * 10)
+    if (gold === location) {
+         document.getElementById("outcome").innerHTML ="💣"}
+    else {} if (randomNumber === 1) { document.getElementById("1").innerHTML ="🌴"}
+    else {} if (randomNumber === 2) { document.getElementById("1").innerHTML ="🌴"}
+    else {} if (randomNumber === 3) { document.getElementById("1").innerHTML ="🌴"}
+    else {} if (randomNumber === 4) { document.getElementById("1").innerHTML ="🌴"}
+    else {} if (randomNumber === 5) { document.getElementById("1").innerHTML ="🌴"}
+    else {} if (randomNumber === 6) { document.getElementById("1").innerHTML ="🌴"}
+    else {} if (randomNumber === 7) { document.getElementById("1").innerHTML ="🌈"}
+    else {} if (randomNumber === 8) { document.getElementById("1").innerHTML ="🌈"}
